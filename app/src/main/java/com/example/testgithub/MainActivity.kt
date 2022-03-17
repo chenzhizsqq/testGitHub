6ebc4b67de64cf90f425bfdf0bf7cb167ca5d3c4
chenzhi <chenzhi@qq.com>
2022-03-17 10:52:53 +0900

    Revert "testFun1"
    This reverts commit 22241a9b43b45428924481a3254c5941381b1f0b.
    
    # Conflicts:
    #	app/src/main/java/com/example/testgithub/MainActivity.kt

--- app/src/main/java/com/example/testgithub/MainActivity.kt
+++ app/src/main/java/com/example/testgithub/MainActivity.kt
@@ -8,12 +8,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    fun testFun1(){
-
-    }
-
-    fun testFun2(){
-
-    }
 }
